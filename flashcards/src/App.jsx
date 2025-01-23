24f3899366972154af4e8f14ec96e48642bc99b8
Liz Wade <11866103+lizwade@users.noreply.github.com>
2025-01-23 19:33:56 +0000

    Data now held as state in App

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -5,8 +5,22 @@ import Form from "./components/Form/Form.jsx";
 
 function App() {
   const [qAndA, setQAndA] = useState([
-    ["What does the fox say?", "Nobody knows"],
-    ["Meaning of life?", "42"],
+    [
+      "What do we call variables that we pass down to other components?",
+      "props",
+    ],
+    [
+      "Why should you use state insted of local variables for values we want to display in components?",
+      "Because otherwise React can't keep track of its value and won't re-render to change what's displayed.",
+    ],
+    [
+      "In what direction does data move in React?",
+      "From parent to child in a unidirectional flow.",
+    ],
+    [
+      "How do you mutate a component's state?",
+      "You should never mutate state - it is read-only. Make a copy if necessary.",
+    ],
   ]);
 
   // first square brackets specify which qAndA. Second specify Question [0] or Answer [1]
@@ -15,26 +29,7 @@ function App() {
   return (
     <>
       <Form />
-      <Flashcards
-        data={[
-          [
-            "What do we call variables that we pass down to other components?",
-            "props",
-          ],
-          [
-            "Why should you use state insted of local variables for values we want to display in components?",
-            "Because otherwise React can't keep track of its value and won't re-render to change what's displayed.",
-          ],
-          [
-            "In what direction does data move in React?",
-            "From parent to child in a unidirectional flow.",
-          ],
-          [
-            "How do you mutate a component's state?",
-            "You should never mutate state - it is read-only. Make a copy if necessary.",
-          ],
-        ]}
-      />
+      <Flashcards data={qAndA} />
     </>
   );
 }
