cf72b04e11c7eeb666bf6cb6758aab31f170dfd9
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2025-01-24 07:30:48 +0000

    Added links to the footer

--- flashcards/src/components/Footer/Footer.jsx
+++ flashcards/src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
+import "./footer.css";
+
 export default function Footer() {
   return (
-    <div className="footer">
-      <p>Built with React ⚛️ | @liz @chris @sami</p>
-    </div>
+    <footer className="footer">
+      <p>
+        Built with React ⚛️ | 
+        <a href="#" target="_blank" rel="noopener noreferrer">@liz</a> | 
+        <a href="https://github.com/CodingOdysseyOrginal" target="_blank" rel="noopener noreferrer">@Christopher</a> | 
+        <a href="#" target="_blank" rel="noopener noreferrer">@sami</a> | 
+        &copy; {new Date().getFullYear()}
+      </p>
+    </footer>
   );
 }
